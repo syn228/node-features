7ecc4ef4528ab4fa160b77006e6a46e65e0a5e7c
Aaron Na <syn228@nyu.edu>
2019-01-12 12:09:18 -0800

    starting multiple worker instances of the cluster to test conditional servers

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
 
-function doWork(duration){
-  const start = Date.now();
-  while(Date.now() - start < duration){
-
-  }
+// File being executed in Master mode?
+if (cluster.isMaster){
+  // execute index.js to create worker instances
+  cluster.fork();
 }
+else {
+  // Child; act as a server
+  const express = require('express');
+  const app = express();
 
-app.get('/', (req, res) => {
-  doWork(5000);
-  res.send("Hello there")
-});
+  function doWork(duration){
+    const start = Date.now();
+    while(Date.now() - start < duration){
 
-app.listen(3000);
+    }
+  }
+
+  app.get('/', (req, res) => {
+    doWork(5000);
+    res.send("Hello there")
+  });
+
+  app.listen(3000);
+
+}
