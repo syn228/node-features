41c61e80075e65c262d301ddbedf5a0a184c7284
Aaron Na <syn228@nyu.edu>
2019-01-13 19:03:42 -0800

    code cleanup to test pm2

--- index.js
+++ index.js
@@ -1,27 +1,21 @@
-const cluster = require('cluster');
+const express = require('express');
+const app = express();
 
-// File being executed in Master mode?
-if (cluster.isMaster){
-  // execute index.js to create worker instances
-  cluster.fork();
-}
-else {
-  // Child; act as a server
-  const express = require('express');
-  const app = express();
+function doWork(duration){
+  const start = Date.now();
+  while(Date.now() - start < duration){
 
-  function doWork(duration){
-    const start = Date.now();
-    while(Date.now() - start < duration){
-
-    }
   }
-
-  app.get('/', (req, res) => {
-    doWork(5000);
-    res.send("Hello there")
-  });
-
-  app.listen(3000);
-
 }
+
+app.get('/', (req, res) => {
+  doWork(5000);
+  res.send("Hello there")
+});
+
+app.get('/fast', (req, res) => {
+  res.send("This is the faster server")
+});
+
+app.listen(3000);
+
